1157b9cacb1d91755257fd4f09e62d5d57d67beb
Ruslan Vlasiuk <ruslan.vlasiuk.0028@gmail.com>
2026-05-31 15:59:14 +0000

    address mentor feedback for hw5

--- src/utils/saveFileToCloudinary.js
+++ src/utils/saveFileToCloudinary.js
@@ -9,12 +9,20 @@ export const saveFileToCloudinary = (buffer) => {
   });
 
   return new Promise((resolve, reject) => {
-    const stream = cloudinary.uploader.upload_stream((error, result) => {
-      if (error) {
-        return reject(error);
-      }
-      resolve(result);
-    });
+    const stream = cloudinary.uploader.upload_stream(
+      {
+        resource_type: 'image',
+        folder: 'avatars',
+        overwrite: true,
+        unique_filename: true,
+      },
+      (error, result) => {
+        if (error) {
+          return reject(error);
+        }
+        resolve(result);
+      },
+    );
 
     Readable.from(buffer).pipe(stream);
   });
